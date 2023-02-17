5262d713c7f0d5d16c4938a608a9c3045662e533
pinkiepupuma <Pinkiepupuma@gmail.com>
2023-02-17 10:37:28 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main-prints the string with proper grammar
+ *
+ * Return: if results are false returns zero
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
