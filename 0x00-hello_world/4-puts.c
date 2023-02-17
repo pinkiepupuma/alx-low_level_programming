e5d6d893566be89525cba51faac381d8374f7137
pinkiepupuma <Pinkiepupuma@gmail.com>
2023-02-17 09:41:13 -0800

    First C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main- is a function that prints the string
+ *
+ * Return: 0 if executed succesfuly or else none if not
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
