45f310e62efdf5d2621276f438b6fc351d9767b3
pinkiepupuma <Pinkiepupuma@gmail.com>
2023-02-22 02:18:43 -0800

    fjd

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -20,10 +20,7 @@ int print_sign(int n)
         }
         else if (n < 0)
         {
-		_putchar(',');
-                _putchar(' ');
-                _putchar(n + '-');
-                _putchar('\n');
+		_putchar('-')
 		return (-1);
         }
 	return (0);
