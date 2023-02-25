d9d55c49d054c48b4dcb14dea1e868463b4d6b9f
pinkiepupuma <Pinkiepupuma@gmail.com>
2023-02-25 07:50:57 -0800

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = "a"; i <= "z"; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
