a1d77cbc2ae06086e112c81f295907ace8258451
pinkiepupuma <Pinkiepupuma@gmail.com>
2023-03-17 16:13:33 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *malloc_checked - functions string
+ *@b: number
+ *Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+void *p;
+
+p = malloc(b);
+if (p == NULL)
+{
+exit(98);
+}
+
+return (p);
+}
