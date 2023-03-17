96b68661f3f32cd3de3b85ab006f1a8c313711c2
pinkiepupuma <Pinkiepupuma@gmail.com>
2023-03-17 16:37:50 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,51 +1,44 @@
 #include "main.h"
 
 /**
- * string_nconcat - concatenates two strings.
+ * string_nconcat - function that concatenates two strings.
+ *
  * @s1: first string
  * @s2: second string
- * @n: index
- * Return: char pointer
+ * @n: memory size
+ *
+ * Return: a pointer to the string array of the 2 strings.
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-	unsigned int size1 = 0, size2 = 0, i;
+	char *s3;
+	unsigned int i, len1, len2, total_len;
+
+	len1 = 0;
+	len2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
+	for (i = 0; s1[i] != '\0'; i++)
+		len1++;
+	for (i = 0; s2[i] != '\0'; i++)
+		len2++;
+	if (n >= len2)
+		n = len2;
 
-	while (s1[size1] != '\0')
-	{
-		size1++;
-	}
+	total_len = len1 + n;
 
-	while (s2[size2] != '\0')
-	{
-		size2++;
-	}
+	s3 = malloc(sizeof(char) * (total_len + 1));
+	if (s3 == NULL)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+		s3[i] = s1[i];
+	for (i = 0; i < n; i++)
+		s3[len1 + i] = s2[i];
+	s3[len1 + i] = '\0';
 
-	if (n > size2)
-	n = size2;
-	p = malloc((size1 + n + 1) * sizeof(char));
-
-	if (p == NULL)
-		return (0);
-
-	for (i = 0; i < size1; i++)
-	{
-		p[i] = s1[i];
-	}
-
-	for (; i < (size1 + n); i++)
-	{
-		p[i] = s2[i - size1];
-	}
-	p[i] = '\0';
-
-return (p);
+	return (s3);
 }
